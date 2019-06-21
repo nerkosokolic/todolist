d7484e49e5bd2de89e05b1cc5d146c1cd586f4e3
Nerko <Nerkosokolic@gmail.com>
2019-06-21 13:40:26 +1000

    layout, logic and styling complete

--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
 // Changes color of Li's if users selected them...
-$('li').click(function (){
+$('ul').on("click", "li", function (){
     $(this).toggleClass("completed");
 })
 
 
 // Click on X to delete line
-$("span").click( function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(300, function(){
         $(this).remove()
     });
     event.stopPropagation();
+});
+
+// 
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13) {
+        //if "enter" key press then grab the text in the input field
+        var todoText = $(this).val();
+        $(this).val("");
+        //create a new li and add to ul
+        $("ul").append("<li><span> x </span>" + todoText + "</li>")
+    }
+    
+})
+
+$(".fa").click(function(){
+    $("input[type='text']").fadeToggle();
 })
